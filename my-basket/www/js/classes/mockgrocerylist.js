80f55b13e1de8a83fb04080ddf00adf96fefa6b4
Tyler Leite <tyler.leite@yandex.com>
2016-04-04 17:06:43 -0400

    Barebones list UI
    Signed-off-by: Tyler Leite <tyler.leite@yandex.com>

---
+++ my-basket/www/js/classes/mockgrocerylist.js
@@ -0,0 +1,31 @@
+if (mb == null || typeof(mb) != "object") { var mb = new Object(); }
+
+mb.MockGroceryListItem = function() {
+	//
+};
+
+mb.listItemToHtml = function() {
+	return '<a href="#">THIS IS A MOCK OBJECT</a>';
+};
+
+/////////////////////////////////////////
+
+mb.MockGroceryList = function() {
+	this.items = [];
+};
+
+mb.MockGroceryList.prototype.load = function(listName) {
+	//Will eventually ping the server for shared lists / local storage for singles
+	this.items.push(new mb.MockGroceryListItem);
+	this.items.push(new mb.MockGroceryListItem);
+	this.items.push(new mb.MockGroceryListItem);
+	this.items.push(new mb.MockGroceryListItem);
+};
+
+mb.MockGroceryList.prototype.get = function(i) {
+	return this.items[i];
+};
+
+mb.MockGroceryList.prototype.len = function() {
+	return this.items.length;
+};
