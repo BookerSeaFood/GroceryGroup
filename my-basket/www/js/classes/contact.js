194ef9ef44f07637eb549cee4ab5e680978dea4e
Tyler Leite <tyler.leite@yandex.com>
2016-04-04 16:43:17 -0400

    Added jQuery libs, work on grocery list html file skeleton
    Signed-off-by: Tyler Leite <tyler.leite@yandex.com>

--- my-basket/www/js/classes/contact.js
+++ my-basket/www/js/classes/contact.js
@@ -6,10 +6,10 @@ mb.SERVER = '127.0.0.1/my-basket/';
 mb.Contact = function() {
 	//Database ID
 	this.uid = -1;
-	
+
 	//Profile of the contact
 	this.profileInfo = new MockUserProfile();
-	
+
 	//Database connection
 	this.db = new MockDatabase();
 };
@@ -17,15 +17,14 @@ mb.Contact = function() {
 
 //TODO: implement create function for inserting into ContactLists
 mb.Contact.prototype.create = function() {
-	
+	//
 };
 
 //TODO: implement in case of user name change
 mb.Contact.prototype.update = function() {
-	
+	//
 };
 
 mb.Contact.prototype.toString = function() {
 	return this.profileInfo.toString();
 };
-
