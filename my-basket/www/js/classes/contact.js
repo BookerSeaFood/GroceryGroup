d92c70a7501b76400d5d6f7543928f3b15057ec4
Nick White <sithjordan@gmail.com>
2016-04-04 18:00:07 -0400

    Edited default constructors

--- my-basket/www/js/classes/contact.js
+++ my-basket/www/js/classes/contact.js
@@ -2,16 +2,14 @@ if (mb == null || typeof(mb) != "object") { var mb = new Object(); }
 
 mb.SERVER = '127.0.0.1/my-basket/';
 
-/** @constructor */
-mb.Contact = function() {
+/**@constructor*/
+mb.Contact = function(un, id) {
 	//Database ID
-	this.uid = -1;
-
-	//Profile of the contact
-	this.profileInfo = new MockUserProfile();
-
+	this.uid = id || -1;
+	
 	//Username of the contact
-	this.username = 'Peter Placeholder';
+	this.username = un || 'Peter Placeholder';
+	
 	//Database connection
 	this.db = new MockDatabase();
 };
@@ -19,14 +17,15 @@ mb.Contact = function() {
 
 //TODO: implement create function for inserting into ContactLists
 mb.Contact.prototype.create = function() {
-	//
+	
 };
 
 //TODO: implement in case of username change
 mb.Contact.prototype.update = function() {
-	//
+	
 };
 
 mb.Contact.prototype.toString = function() {
-	return this.profileInfo.toString();
+	return this.username.toString();
 };
+
