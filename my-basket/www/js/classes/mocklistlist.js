762b967bdea0eb0d80b367c703a6477aeeb4e7d2
Tyler Leite <tyler.leite@yandex.com>
2016-04-04 16:10:06 -0400

    Removed unnecessary files, moved img into res
    Signed-off-by: Tyler Leite <tyler.leite@yandex.com>

--- my-basket/www/js/classes/mocklistlist.js
+++ my-basket/www/js/classes/mocklistlist.js
@@ -1,3 +1,13 @@
 if (mb == null || typeof(mb) != "object") { var mb = new Object(); }
 
-var mb.MockListList = function() {};
+var mb.MockListList = function() {
+	this._lists = [];
+};
+
+var mb.MockListList.prototype.add(ls) = function() {
+	this._lists.push(ls);
+};
+
+var mb.MockListList.prototype.lists() = function() {
+	return this._lists;
+}
