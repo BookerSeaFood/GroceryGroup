03cd4c237cb914d0d17d9be4a4d4325c45a24384
Tyler Leite <tyler.leite@yandex.com>
2016-04-06 21:41:33 -0400

    styled localstoragemanager code
    Signed-off-by: Tyler Leite <tyler.leite@yandex.com>

--- my-basket/www/js/classes/localstoragemanager.js
+++ my-basket/www/js/classes/localstoragemanager.js
@@ -1,131 +1,67 @@
 if (mb == null || typeof(mb) != "object") { var mb = new Object(); }
 
-mb.fakeStorage = {
-  _data: {},
-
-  setItem: function (id, val) {
-    return this._data[id] = String(val);
-  },
-
-  getItem: function (id) {
-    return this._data.hasOwnProperty(id) ? this._data[id] : undefined;
-  },
-
-  removeItem: function (id) {
-    return delete this._data[id];
-  },
-
-  clear: function () {
-    return this._data = {};
-  }
+mb.FakeStorage = function() {
+	this._data: new Object();
 };
 
-function mb.LocalStorageManager() {
-  this.UserIDKey     = "UserID";
-  this.listKey     = "list";
+mb.FakeStorage.prototype.setItem = function(id, val) {
+	return this._data[id] = String(val);
+},
 
-  var supported = this.localStorageSupported();
-  this.storage = supported ? window.localStorage : window.fakeStorage;
-}
-
-mb.LocalStorageManager.prototype.localStorageSupported = function () {
-  var testKey = "test";
-  var storage = window.localStorage;
-
-  try {
-    storage.setItem(testKey, "1");
-    storage.removeItem(testKey);
-    return true;
-  } catch (error) {
-    return false;
-  }
+mb.FakeStorage.prototype.getItem = function(id) {
+	return this._data.hasOwnProperty(id) ? this._data[id] : undefined;
 };
 
-// Best score getters/setters
-mb.LocalStorageManager.prototype.getUserID = function () {
-  return this.storage.getItem(this.UserIDKey) || 0;
+mb.FakeStorage.prototype.removeItem = function(id) {
+	return delete this._data[id];
 };
 
-mb.LocalStorageManager.prototype.setUserID = function (ID) {
-  this.storage.setItem(this.UserIDKey, ID);
+mb.FakeStorage.prototype.clear = function() {
+	return this._data = {};
 };
 
-// Game state getters/setters and clearing
-mb.LocalStorageManager.prototype.getList = function () {
-  var stateJSON = this.storage.getItem(this.listKey);
-  return stateJSON ? JSON.parse(stateJSON) : null;
+////////////////////////////////////////////////////////////////////////////////
+
+mb.LocalStorageManager = function() {
+	this.userIdKey = "userId";
+	this.listKey = "list";
+
+	var supported = this.localStorageSupported();
+	this.storage = supported ? window.localStorage : window.fakeStorage;
 };
 
-mb.LocalStorageManager.prototype.setList = function (theList) {
-  this.storage.setItem(this.listKey, JSON.stringify(theList));
-};
+mb.LocalStorageManager.prototype.localStorageSupported = function() {
+	var testKey = "test";
+	var storage = window.localStorage;
 
-mb.LocalStorageManager.prototype.clearUserID = function () {
-  this.storage.removeItem(this.UserIDKey);
-};
-
-if (mb == null || typeof(mb) != "object") { var mb = new Object(); }
-
-window.fakeStorage = {
-  _data: {},
-
-  setItem: function (id, val) {
-    return this._data[id] = String(val);
-  },
-
-  getItem: function (id) {
-    return this._data.hasOwnProperty(id) ? this._data[id] : undefined;
-  },
-
-  removeItem: function (id) {
-    return delete this._data[id];
-  },
-
-  clear: function () {
-    return this._data = {};
-  }
-};
-
-function LocalStorageManager() {
-  this.bestScoreKey     = "bestScore";
-  this.gameStateKey     = "gameState";
-
-  var supported = this.localStorageSupported();
-  this.storage = supported ? window.localStorage : window.fakeStorage;
-}
-
-LocalStorageManager.prototype.localStorageSupported = function () {
-  var testKey = "test";
-  var storage = window.localStorage;
-
-  try {
-    storage.setItem(testKey, "1");
-    storage.removeItem(testKey);
-    return true;
-  } catch (error) {
-    return false;
-  }
+	try {
+		storage.setItem(testKey, "1");
+		storage.removeItem(testKey);
+		return true;
+	} catch (error) {
+		return false;
+	}
 };
 
 // Best score getters/setters
-LocalStorageManager.prototype.getBestScore = function () {
-  return this.storage.getItem(this.bestScoreKey) || 0;
+mb.LocalStorageManager.prototype.getUserId = function() {
+	return this.storage.getItem(this.userIdKey) || 0;
 };
 
-LocalStorageManager.prototype.setBestScore = function (score) {
-  this.storage.setItem(this.bestScoreKey, score);
+mb.LocalStorageManager.prototype.setUserId = function(id) {
+	this.storage.setItem(this.userIdKey, id);
 };
 
 // Game state getters/setters and clearing
-LocalStorageManager.prototype.getGameState = function () {
-  var stateJSON = this.storage.getItem(this.gameStateKey);
-  return stateJSON ? JSON.parse(stateJSON) : null;
+mb.LocalStorageManager.prototype.getList = function() {
+	var stateJSON = this.storage.getItem(this.listKey);
+	return stateJSON ? JSON.parse(stateJSON) : null;
 };
 
-LocalStorageManager.prototype.setGameState = function (gameState) {
-  this.storage.setItem(this.gameStateKey, JSON.stringify(gameState));
+mb.LocalStorageManager.prototype.setList = function(theList) {
+	this.storage.setItem(this.listKey, JSON.stringify(theList));
 };
 
-LocalStorageManager.prototype.clearGameState = function () {
-  this.storage.removeItem(this.gameStateKey);
+mb.LocalStorageManager.prototype.clearuserId = function() {
+	this.storage.removeItem(this.userIdKey);
 };
