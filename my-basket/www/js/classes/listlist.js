b156a9421de4d4ceae91dfb5fc2a07648a82d6f6
Nick White <sithjordan@gmail.com>
2016-04-23 13:13:30 -0400

    Changed MockListList to ListList and added listlist.js

---
+++ my-basket/www/js/classes/listlist.js
@@ -0,0 +1,32 @@
+if (mb == null || typeof(mb) != 'object') { var mb = new Object(); }
+
+mb.ListList = function(id) {
+	this._lists = [];
+	this.id = id;
+};
+
+mb.ListList.prototype.get = function(i) {
+	return this._lists[i];
+}
+
+mb.ListList.prototype.add = function(ls) {
+	this._lists.push(ls);
+};
+
+mb.ListList.prototype.lists = function() {
+	return this._lists;
+};
+
+mb.ListList.prototype.len = function() {
+	return this._lists.length;
+};
+
+mb.ListList.listByName = function(that, str) {
+	for (var i = 0; i < that._lists.length; i++){
+		if (that._lists[i].name === str){
+			return that._lists[i];
+		}
+	}
+
+	return null;
+};
