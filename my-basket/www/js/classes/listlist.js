4064ca29f2008c0da297e6d8e6fc1196a814f734
Nick White <sithjordan@gmail.com>
2016-04-27 23:18:04 -0400

    Added remove list feature

--- my-basket/www/js/classes/listlist.js
+++ my-basket/www/js/classes/listlist.js
@@ -44,6 +44,24 @@ mb.ListList.prototype.len = function() {
 	return this._lists.length;
 };
 
+mb.ListList.prototype.removeList = function(that, listName) {
+	var checksum = 0;
+	//Possible remove later, could be redundant
+	if (that._lists.length === 1) {
+		that._lists.splice(0);
+	}
+	for (i=0; i < that._lists.length; i++) {
+	  	if (that._lists[i].getName() === listName) {
+	    	that._lists.splice(i, 1);
+	    	checksum = 1;
+	    }
+		if (checksum === 1 && i !== that._lists.length) {
+			that._lists[i].id = i;
+		}
+	}
+  	return 0;
+};
+
 mb.ListList.listByName = function(that, str) {
 	for (var i = 0; i < that._lists.length; i++){
 		if (that._lists[i].name === str){
