4dad55237694f7f9c97f8fc24349aa6587336111
Tyler Leite <tyler.leite@yandex.com>
2016-04-27 23:01:44 -0400

    More ui work

--- my-basket/www/js/classes/listlist.js
+++ my-basket/www/js/classes/listlist.js
@@ -3,41 +3,23 @@ if (mb == null || typeof(mb) != 'object') { var mb = new Object(); }
 mb.ListList = function(id) {
 	this._lists = [];
 	this.id = id;
-	/*
-	if (localStorage.getItem("checklist") == 26){
-		var stateJSON = localStorage.getItem("list");
-		this._lists = stateJSON ? JSON.parse(stateJSON) : null;
-		this.id = id;
-	} else {
-	*/
-	//localStorage.setItem("checklist", 26);
 };
 
 
 mb.ListList.prototype.get = function(i) {
 	return this._lists[i];
-	//return LocalStorageManager.getList();
-
-	//var stateJSON = this.storage.getItem(this.listKey);
-	//return stateJSON ? JSON.parse(stateJSON) : null;
-
-
 }
 
 mb.ListList.prototype.add = function(ls) {
 	this._lists.push(ls);
-
-
-	//localStorage.setItem("list", JSON.stringify(this._lists));
 };
 
 mb.ListList.prototype.lists = function() {
 	return this._lists;
-	//return LocalStorageManager.getList();
 };
 
 mb.ListList.prototype.update = function(){
-	//localStorage.setItem("list", JSON.stringify(this._lists));
+	//
 }
 
 mb.ListList.prototype.len = function() {
